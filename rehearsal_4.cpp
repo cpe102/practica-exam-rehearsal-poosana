c535e8a2e32193068edabf4b4b1c26f10b0d7cb7
neokarn <34811101+neokarn@users.noreply.github.com>
2020-01-10 16:37:59 +0700

    Initial commit

---
+++ rehearsal_4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//Write compress() here.
+
+int main()
+{
+    string a = compress("ABCDEFGHIJKLMN");
+    string b = compress("123456");
+    string c = compress("HelloWorld");
+    string d = compress("BNK48");
+    string e = compress("COMPROG261102");
+    string f = compress("A");
+    string g = compress("AB");
+    string h = compress("ABC");
+    string i = compress("ABCD");
+    
+    cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n" << h << "\n" << i << "\n";
+
+}
